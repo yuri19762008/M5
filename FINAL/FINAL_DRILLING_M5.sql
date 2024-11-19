1632bc328983a32ba572055e6ed4d5dd48e499b3
Yuri Urzua Lebuy <yuri19762008@hotmail.com>
2024-11-19 20:38:10 -0300

    fix

--- FINAL/FINAL_DRILLING_M5.sql
+++ FINAL/FINAL_DRILLING_M5.sql
@@ -247,4 +247,21 @@ WHERE
     f.release_year = 2006
     AND f.rental_rate > 4.0;
 
-
+select
+  t1.TABLE_NAME AS tabla_nombre, 
+  t1.COLUMN_NAME AS columna_nombre, 
+  t1.COLUMN_DEFAULT AS columna_defecto, 
+  t1.IS_NULLABLE AS columna_nulo,
+  t1.DATA_TYPE AS columna_tipo_dato, 
+  COALESCE(t1.NUMERIC_PRECISION,
+  t1.CHARACTER_MAXIMUM_LENGTH) AS columna_longitud,
+  PG_CATALOG.COL_DESCRIPTION(t2.OID, 
+  t1.DTD_IDENTIFIER::int) AS columna_descripcion,
+  t1.DOMAIN_NAME AS columna_dominio 
+FROM 
+	INFORMATION_SCHEMA.COLUMNS t1 
+	INNER JOIN PG_CLASS t2 ON (t2.RELNAME = t1.TABLE_NAME)
+	WHERE t1.TABLE_SCHEMA = 'public'
+ORDER BY 
+	t1.TABLE_NAME; 
+   
